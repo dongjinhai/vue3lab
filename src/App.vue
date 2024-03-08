66c1c2dada31598bcd7aad8fd366590cfa4a8013
jinhaidong <jinhaidong@tencent.com>
2024-03-08 18:36:46 +0800

    提交代码

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,18 @@ const refRoutes = ref(routes);
 
 <template>
   <div class="flex justify-between w-full h-full">
-    <div class="bg-slate-400 flex flex-col h-full w-24">
+    <div class="bg-slate-400 flex flex-col h-full min-w-[150px]">
       <router-link
         v-for="rou in refRoutes"
         :key="rou.name"
         :to="rou.path"
-        class="text-center m-2"
+        class="text-center m-2 bg-blue-400 rounded-md p-1"
+        tag="button"
       >
         {{ rou.name }}</router-link
       >
     </div>
-    <div class="bg-slate-400 flex-1">
+    <div class="bg-gray-300 flex-1 p-2">
       <router-view></router-view>
     </div>
   </div>
