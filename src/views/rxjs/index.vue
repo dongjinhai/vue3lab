4f1f16c1a9eb854273b55b26471e4f14882dc845
jinhaidong <1650152108@qq.com>
2025-01-24 21:56:21 +0800

    添加测试

---
+++ src/views/rxjs/index.vue
@@ -0,0 +1,14 @@
+<script setup lang='ts'>
+import { observable } from 'rxjs';
+defineOptions({
+  name: 'Rxjs'
+})
+</script>
+
+<template>
+  <div>test</div>
+</template>
+
+<style lang='scss' scoped>
+
+</style>
